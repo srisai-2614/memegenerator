442ad57e6f1bb842831eda7b6c039dd8fb69dbb5
Prasad Madine <19pa1a0595@vishnu.edu.in>
2023-09-01 13:49:51 +0530

    my first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src=".\images\meme.jpg" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            
+        </header>
+    )
+}
